659f41722c09e2925325f649d5498c656f35c7ff
atn-mrgn <77300450+atn-mrgn@users.noreply.github.com>
2021-01-12 20:38:16 +0100

    Update urls.py

--- replace/replace/urls.py
+++ replace/replace/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('mrgn/', admin.site.urls),
     path('', include('store.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
